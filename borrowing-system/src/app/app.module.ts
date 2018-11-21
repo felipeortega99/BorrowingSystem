332edc9b738f73e783a0a395566d85975f4d150c
Mariana <marianacazaresm@gmail.com>
2018-11-20 18:30:07 -0800

    Components created and bootstrap 4 added

--- borrowing-system/src/app/app.module.ts
+++ borrowing-system/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { FinesComponent } from './fines/fines.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddProductComponent,
+    FinesComponent
   ],
   imports: [
     BrowserModule,

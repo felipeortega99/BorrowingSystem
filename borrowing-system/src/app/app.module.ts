4fbe948a2b6afde1319fb4254f4fcdb84db55c31
Felipe Ortega Casillas <felipeorcas@gmail.com>
2018-11-25 21:48:39 -0800

    Home view finished

--- borrowing-system/src/app/app.module.ts
+++ borrowing-system/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Routes
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
-import { HomeComponent, 
-  FinesComponent, 
-  LoginComponent, 
-  AddProductComponent 
+import { HomeComponent,
+  FinesComponent,
+  LoginComponent,
+  AddProductComponent
 } from './components/index.components';
 
 @NgModule({

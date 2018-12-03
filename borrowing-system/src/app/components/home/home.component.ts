0145106c16900432fb6ab5092e1111d03405cf8a
Mariana <marianacazaresm@gmail.com>
2018-12-03 02:15:52 -0800

    Firebase added to home

--- borrowing-system/src/app/components/home/home.component.ts
+++ borrowing-system/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../services/auth.service";
 import { Router } from "@angular/router";
 import { AngularFireAuth } from "@angular/fire/auth";
@@ -18,22 +18,19 @@ export class HomeComponent implements OnInit {
   products: any;
   private loan = {} as LoanModel;
 
-  constructor(private formBuilder: FormBuilder,
+  constructor(
+    private formBuilder: FormBuilder,
     private authService: AuthService,
     private router: Router,
     private afAuth: AngularFireAuth,
-    private afDb: AngularFireDatabase ) {
+    private afDb: AngularFireDatabase
+  ) {
     this.createForm();
     this.afAuth.authState.subscribe(auth => {
       if (auth) {
         this.isLogged = true;
         console.log("Got to home. We are logged in");
-        afDb
-          .list("/products")
-          .valueChanges()
-          .subscribe(products => {
-            this.products = products;
-          });
+        this.loadData();
       } else {
         this.isLogged = false;
         console.log("Got to home but wasn't logged in");
@@ -44,13 +41,25 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
+  loadData() {
+    //Get available products
+    this.afDb
+      .list("/products", ref => ref.orderByChild("available").equalTo(true))
+      .valueChanges()
+      .subscribe(products => {
+        this.products = products;
+      });
+  }
+
   createForm() {
     this.rentProductForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      studentNumber: ['', Validators.required]
+      name: ["", Validators.required],
+      studentNumber: ["", Validators.required]
     });
   }
-  get f() { return this.rentProductForm.controls; }
+  get f() {
+    return this.rentProductForm.controls;
+  }
 
   onSubmit() {
     this.submitted = true;
